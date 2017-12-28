9c7b49274eb425050eaff23b3ea73214f2643a92
Bartosz Miera <bartoszmiera@gmail.com>
2017-12-28 23:52:45 +0100

    removed dead code

--- app/src/main/kotlin/pl/polsl/student/personalnavigation/BackendMarkersSource.kt
+++ app/src/main/kotlin/pl/polsl/student/personalnavigation/BackendMarkersSource.kt
@@ -1,26 +1,25 @@
 package pl.polsl.student.personalnavigation
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.kittinunf.fuel.Fuel
 import org.osmdroid.util.BoundingBox
 
 class BackendMarkersSource(private val serverUrl: String) : MarkersSource {
 
     override fun getMarker(id: Long): IdentifiableMarker {
-        return GetRequestFactory(
+        return Fuel.get(
                         endpointUrl("/marker/$id")
                 )
-                .createRequest()
                 .responseJsonOrThrow<DefaultIdentifiableMarker>()
     }
 
     override fun getMarkersIn(boundingBox: BoundingBox): Iterable<IdentifiableMarker> {
-        return GetRequestFactory(
+        return Fuel.get(
                         endpointUrl("/markers"),
                         listOf(
                                 "boundingBox" to jacksonObjectMapper().writeValueAsString(BackendBoundingBox(boundingBox))
                         )
                 )
-                .createRequest()
                 .responseJsonOrThrow<Array<DefaultIdentifiableMarker>>()
                 .toList()
     }
--- app/src/main/kotlin/pl/polsl/student/personalnavigation/RequestFactory.kt
+++
@@ -1,7 +0,0 @@
-package pl.polsl.student.personalnavigation
-
-import com.github.kittinunf.fuel.core.Request
-
-interface RequestFactory {
-    fun createRequest() : Request
-}
